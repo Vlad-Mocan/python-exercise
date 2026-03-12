7c6ca1fc55ba622c4e32c44e970b1c85dd088aab
Vlad-Andrei Mocan <Vlad.Mocan@cognizant.com>
2026-03-12 13:37:29 +0200

    rest api finished

---
+++ fifth-homework-classes/homework/model/impl/task.py
@@ -0,0 +1,7 @@
+from model.entity import Entity
+
+
+class Task(Entity):
+    def add(self, title: str, description: str, completed: bool) -> None:
+        values = [title, description, completed]
+        super().add(values)
